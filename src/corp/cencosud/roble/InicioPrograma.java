b45e55023f6f18cc8157ac920ef5eab6be543dc5
cencosudweb.panel <cencosudweb.panel@gmail.com>
2017-12-28 17:59:47 -0300

    Cambios

---
+++ src/corp/cencosud/roble/InicioPrograma.java
@@ -0,0 +1,372 @@
+/**
+ *@name InicioPrograma.java
+ * 
+ *@version 1.0 
+ * 
+ *@date 31-03-2017
+ * 
+ *@author EA7129
+ * 
+ *@copyright Cencosud. All rights reserved.
+ */
+package corp.cencosud.roble;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.HashMap;
+import java.util.Map;
+/**
+ * @description 
+ */
+public class InicioPrograma {
+	
+	private static BufferedWriter bw;
+	private static String path;
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Map <String, String> mapArguments = new HashMap<String, String>();
+		String sKeyAux = null;
+
+		for (int i = 0; i < args.length; i++) {
+
+			if (i % 2 == 0) {
+
+				sKeyAux = args[i];
+			}
+			else {
+
+				mapArguments.put(sKeyAux, args[i]);
+			}
+		}
+
+		try {
+
+			File info              = null;
+			File miDir             = new File(".");
+			path                   =  miDir.getCanonicalPath();
+			info                   = new File(path+"/info.txt");
+			bw = new BufferedWriter(new FileWriter(info));
+			info("El programa se esta ejecutando...");
+			crearTxt(mapArguments);
+			System.out.println("El programa finalizo.");
+			info("El programa finalizo.");
+			bw.close();
+
+		}
+		catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+	}
+	
+	private static void crearTxt(Map <String, String> mapArguments) {
+
+		Connection dbconnOracle    = crearConexionOracle();
+		File file1              = null;
+		BufferedWriter bw       = null;
+		PreparedStatement pstmt = null;
+		StringBuffer sb         = null;
+		
+		String iFechaIni        = null;
+		String iFechaFin        = null;
+
+		try {
+
+			try {
+
+				iFechaFin = restarDia(mapArguments.get("-fi"));
+				iFechaIni = restarSieteDia(mapArguments.get("-fi"));
+				//iFechaFin = "23-05-2017";//restarDia(mapArguments.get("-fi"));
+				//iFechaIni = "30-05-2017";//restarSieteDia(mapArguments.get("-fi"));
+				
+				System.out.println("iFechaIni: "+iFechaIni);
+				System.out.println("iFechaFin: "+iFechaFin);
+
+			}
+			catch (Exception e) {
+
+				e.printStackTrace();
+			}
+			
+			file1                   = new File(path + "/WMS-" + iFechaFin + ".txt");
+			
+			sb = new StringBuffer();
+			sb.append("SELECT ");
+			sb.append("PT.MARK_FOR || '-' || IM.SIZE_DESC AS LLAVE, ");
+			sb.append("PT.MARK_FOR AS NRO_ORDEN, ");
+			sb.append("DT.PKT_CTRL_NBR AS PICK_TICKET,");
+			sb.append("CH.WAVE_NBR AS NRO_OLA,");
+			//sb.append("TO_CHAR(WP.CREATE_DATE_TIME, 'DD-MM-YYYY HH24:MI:SS') AS FECHA_INICIO_OLA,");
+			sb.append("NVL(TO_CHAR(WP.CREATE_DATE_TIME, 'DD-MM-YYYY HH24:MI:SS'),'01-01-1999 00:00:01') AS FECHA_INICIO_OLA,");
+			
+			sb.append("TO_CHAR(WP.WAVE_STAT_DATE_TIME, 'DD-MM-YYYY HH24:MI:SS') AS FECHA_TERMINO_OLA,");
+			sb.append("LD.LOAD_NBR AS NRO_CARGA,");
+			sb.append("NVL(TO_CHAR(LD.LAST_INVC_DATE_TIME, 'DD-MM-YYYY HH24:MI:SS'),'01-01-1999 00:00:01') AS FECHA_CIERRE_CARGA,");
+			//sb.append("TO_CHAR(WP.WAVE_STAT_DATE_TIME, 'DD-MM-YYYY HH24:MI:SS') AS FECHA_TERMINO_OLA,");
+			
+			sb.append("IM.SIZE_DESC AS SKU,  ");
+			sb.append("TO_CHAR(PT.CREATE_DATE_TIME, 'DD-MM-YYYY HH24:MI:SS') AS FEC_CREA_HD_PKT, ");
+			sb.append("TO_CHAR(PT.MOD_DATE_TIME, 'DD-MM-YYYY HH24:MI:SS') AS FEC_MOD_HD_PKT,");
+			sb.append("DT.PKT_SEQ_NBR AS PKT_SEQ_NBR, ");
+			sb.append("DT.ORIG_PKT_QTY AS CANT_PKT ");
+			sb.append("FROM PKT_HDR PT ");
+			sb.append("INNER JOIN PKT_DTL DT  ON PT.PKT_CTRL_NBR = DT.PKT_CTRL_NBR ");
+			sb.append("INNER JOIN ITEM_MASTER IM ON DT.SKU_ID = IM.SKU_ID ");
+			sb.append("INNER JOIN CARTON_HDR CH ON PT.PKT_CTRL_NBR = CH.PKT_CTRL_NBR ");
+			sb.append("INNER JOIN CARTON_DTL CD ON CH.CARTON_NBR = CD.CARTON_NBR ");
+			sb.append("INNER JOIN WAVE_PARM WP ON WP.WAVE_NBR = CH.WAVE_NBR ");
+			sb.append("INNER JOIN OUTBD_LOAD LD ON CH.LOAD_NBR = LD.LOAD_NBR ");
+			sb.append("WHERE ");
+			sb.append("PT.MARK_FOR IS NOT NULL AND ");
+			sb.append("WP.CREATE_DATE_TIME BETWEEN ");
+			sb.append("TO_DATE('");
+			sb.append(iFechaIni);
+			sb.append(" 00:00:00");
+			sb.append("', 'DD-MM-YYYY HH24:MI:SS') AND ");
+			sb.append("TO_DATE('");
+			sb.append(iFechaFin);
+			sb.append(" 23:59:59");
+			sb.append("', 'DD-MM-YYYY HH24:MI:SS') ");
+			sb.append("GROUP BY ");
+			sb.append("PT.MARK_FOR || '-' || IM.SIZE_DESC,  ");
+			sb.append("PT.MARK_FOR, ");
+			sb.append("DT.PKT_CTRL_NBR, ");
+			sb.append("CH.WAVE_NBR, ");
+			sb.append("TO_CHAR(WP.CREATE_DATE_TIME, 'DD-MM-YYYY HH24:MI:SS'), ");
+			sb.append("TO_CHAR(WP.WAVE_STAT_DATE_TIME, 'DD-MM-YYYY HH24:MI:SS'), ");
+			sb.append("LD.LOAD_NBR, ");
+			sb.append("TO_CHAR(LD.LAST_INVC_DATE_TIME, 'DD-MM-YYYY HH24:MI:SS'), ");
+			sb.append("IM.SIZE_DESC,  ");
+			sb.append("PT.CREATE_DATE_TIME, PT.MOD_DATE_TIME, ");
+			sb.append("DT.CREATE_DATE_TIME, DT.MOD_DATE_TIME, ");
+			sb.append("DT.PKT_SEQ_NBR, DT.ORIG_PKT_QTY ");
+			info("sb="+sb);
+
+			pstmt        = dbconnOracle.prepareStatement(sb.toString());
+			sb           = new StringBuffer();
+			ResultSet rs = pstmt.executeQuery();
+			bw           = new BufferedWriter(new FileWriter(file1));
+ 
+			bw.write("LLAVE;");
+			bw.write("NRO_ORDEN;");
+			bw.write("PICK_TICKET;");
+			bw.write("NRO_OLA;");
+			bw.write("FECHA_INICIO_OLA;");
+			bw.write("FECHA_TERMINO_OLA;");
+			bw.write("NRO_CARGA;");
+			bw.write("FECHA_CIERRE_CARGA;");
+			bw.write("SKU;");
+			bw.write("FEC_CREA_HD_PKT;");
+			bw.write("FEC_MOD_HD_PKT;");
+			bw.write("PKT_SEQ_NBR;");
+			bw.write("CANT_PKT;\n");
+
+			while (rs.next()) {
+				
+				bw.write(rs.getString("LLAVE") + ";");
+				bw.write(rs.getString("NRO_ORDEN") + ";");
+				bw.write(rs.getString("PICK_TICKET") + ";");
+				bw.write(rs.getString("NRO_OLA") + ";");
+				bw.write(rs.getString("FECHA_INICIO_OLA") + ";");
+				bw.write(rs.getString("FECHA_TERMINO_OLA") + ";");
+				bw.write(rs.getString("NRO_CARGA") + ";");
+				bw.write(rs.getString("FECHA_CIERRE_CARGA") + ";");
+				bw.write(rs.getString("SKU") + ";");
+				bw.write(rs.getString("FEC_CREA_HD_PKT") + ";");
+				bw.write(rs.getString("FEC_MOD_HD_PKT") + ";");
+				bw.write(rs.getString("PKT_SEQ_NBR") + ";");
+				bw.write(rs.getString("CANT_PKT") + "; \n");
+			
+			}
+
+			info("Archivos creados.");
+		}
+		catch (Exception e) {
+
+			info("[crearTxt1]Exception:"+e.getMessage());
+			
+		}
+		finally {
+
+			cerrarTodo(dbconnOracle,pstmt,bw);
+			
+		}
+	}
+	
+	private static Connection crearConexionOracle() {
+		System.out.println("Creando conexion a WMS.");
+
+		Connection dbconnection = null;
+
+		try {
+
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			
+			//El servidor g500603sv0zt corresponde a ProducciÃ³n.
+			dbconnection = DriverManager.getConnection("jdbc:oracle:thin:@g500603svbbr:1521:REPORTMHN","CONWMS","CONWMS");
+			System.out.println("Conexion a WMS CREADA.");
+		}
+		catch (Exception e) {
+
+			e.printStackTrace();
+		}
+		return dbconnection;
+	}
+
+	/**
+	 * Metodo que cierra la conexion, Procedimintos,  BufferedWriter
+	 * 
+	 * @param Connection,  Objeto que representa una conexion a la base de datos
+	 * @param PreparedStatement, Objeto que representa una instrucción SQL precompilada. 
+	 * @return retorna
+	 * 
+	 */
+	private static void cerrarTodo(Connection cnn, PreparedStatement pstmt, BufferedWriter bw){
+
+		try {
+
+			if (cnn != null) {
+
+				cnn.close();
+				cnn = null;
+			}
+		}
+		catch (Exception e) {
+
+			System.out.println(e.getMessage());
+			info("[cerrarTodo]Exception:"+e.getMessage());
+		}
+		try {
+
+			if (pstmt != null) {
+
+				pstmt.close();
+				pstmt = null;
+			}
+		}
+		catch (Exception e) {
+
+			System.out.println(e.getMessage());
+			info("[cerrarTodo]Exception:"+e.getMessage());
+		}
+		try {
+
+			if (bw != null) {
+
+				bw.flush();
+				bw.close();
+				bw = null;
+			}
+		}
+		catch (Exception e) {
+
+			System.out.println(e.getMessage());
+			info("[cerrarTodo]Exception:"+e.getMessage());
+		}
+	}
+	
+	/**
+	 * Metodo que muestra informacion 
+	 * 
+	 * @param String, texto a mostra
+	 * @param String, cantidad para restar dias
+	 * @return String retorna los dias a restar
+	 * 
+	 */
+	private static void info(String texto){
+
+		try {
+
+			bw.write(texto+"\n");
+			bw.flush();
+		}
+		catch (Exception e) {
+
+			System.out.println("Exception:"+e.getMessage());
+		}
+	}
+
+	/**
+	 * Metodo que resta dias segun dia ingresado
+	 * 
+	 * @param String, dia que se resta
+	 * @param String, cantidad para restar dias
+	 * @return String retorna los dias a restar
+	 * 
+	 */
+	private static String restarDia(String sDia) {
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy");
+
+		String dia = "";
+		String sFormato = "yyyyMMdd";
+		Calendar diaAux = null;
+		//String sDiaAux = null;
+		SimpleDateFormat df = null;
+
+		try {
+
+			diaAux = Calendar.getInstance();
+			df = new SimpleDateFormat(sFormato);
+			diaAux.setTime(df.parse(sDia));
+			//diaAux.add(Calendar.DAY_OF_MONTH, -1);
+			//sDiaAux = df.format(diaAux.getTime());
+			//dia = Integer.parseInt(sDiaAux);
+			dia = sdf.format(diaAux.getTime());
+			
+		}
+		catch (Exception e) {
+
+			info("[restarDia]Exception:"+e.getMessage());
+		}
+
+		return dia;
+	}
+	
+	/**
+	 * Metodo que resta 7 dias a la fecha ingresada
+	 * 
+	 * @param String, fecha ingresada
+	 * @return String retorna 7 dias atras a la fecha ingresada
+	 * 
+	 */
+	private static String restarSieteDia(String sDia) {
+
+		SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy");
+		
+		String dia = "";
+		String sFormato = "yyyyMMdd";
+		Calendar diaAux = null;
+		//String sDiaAux = null;
+		SimpleDateFormat df = null;
+
+		try {
+
+			diaAux = Calendar.getInstance();
+			df = new SimpleDateFormat(sFormato);
+			diaAux.setTime(df.parse(sDia));
+			diaAux.add(Calendar.DAY_OF_MONTH, -7);
+			//sDiaAux = df.format(diaAux.getTime());
+			//dia = Integer.parseInt(sDiaAux);
+			
+			dia = sdf.format(diaAux.getTime());
+			
+		}
+		catch (Exception e) {
+
+			info("[restarDia]Exception:"+e.getMessage());
+		}
+		return dia;
+	}
+
+}
